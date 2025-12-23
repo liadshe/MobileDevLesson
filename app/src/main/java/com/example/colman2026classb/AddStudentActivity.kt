33bf01c2cc6b5bfcef32a32acc0404e3bddc590b
liadshe <liadshe1@gmail.com>
2025-12-23 13:19:22 +0200

    add student activity

--- app/src/main/java/com/example/colman2026classb/AddStudentActivity.kt
+++ app/src/main/java/com/example/colman2026classb/AddStudentActivity.kt
@@ -1,6 +1,9 @@
 package com.example.colman2026classb
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,26 @@ class AddStudentActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val addStudentButton: Button = findViewById(R.id.add_student_save_student_button)
+        val cancelButton: Button = findViewById(R.id.add_student_cancel_button)
+
+        val studentNameEditText: EditText = findViewById(R.id.add_student_name_editText)
+        val studentIdEditText: EditText = findViewById(R.id.add_student_id_editText)
+
+        val saveStatusTextView: TextView = findViewById(R.id.add_student_save_status_textView)
+        cancelButton.setOnClickListener {
+            finish()
+        }
+
+        addStudentButton.setOnClickListener {
+            val studentName = studentNameEditText.text.toString()
+            val studentId = studentIdEditText.text.toString()
+
+            saveStatusTextView.text = "Student Saved: Name - $studentName, ID - $studentId"
+            studentNameEditText.text.clear()
+            studentIdEditText.text.clear()
         }
     }
 }
