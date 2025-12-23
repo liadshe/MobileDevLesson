6f0c57738d007b4d0923b3c0f6b61a19b31e7863
liadshe <liadshe1@gmail.com>
2025-12-23 12:53:00 +0200

    initial commit

---
+++ app/src/main/java/com/example/colman2026classb/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.colman2026classb
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val button = findViewById<Button>(R.id.button1)
+
+
+        button.setOnClickListener {
+            Log.d("MainActivity", "Button clicked!")
+            val intent = Intent(this, AddStudentActivity::class.java)
+            startActivity(intent)
+        }}
+
+    }
